30d6f15dc5d6787d8b5f5f6c10fb66d8d6dfc5a3
EDIEUSDI BIN AHMAD <edieusdi@gmail.com>
2026-06-20 12:27:33 +0800

    feat: add header files

--- philo.h
+++ philo.h
@@ -6,7 +6,24 @@
 /*   By: ebin-ahm <ebin-ahm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/11 10:28:13 by ebin-ahm          #+#    #+#             */
-/*   Updated: 2026/06/18 21:07:42 by ebin-ahm         ###   ########.fr       */
+/*   Updated: 2026/06/20 12:26:11 by ebin-ahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <limits.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+#define FORK_MSG "has taken a fork"
+#define EAT_MSG "is eating"
+#define SLEEP_MSG "is sleeping"
+#define THINK_MSG "is thinking"
+#define DEATH_MSG "died"
+
+#endif
